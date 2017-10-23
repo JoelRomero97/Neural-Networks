1df9a0351ccbc17fb596b3775cfd2f23878b341b
Joel Romero <joelrg1288@gmail.com>
2017-10-22 20:14:12 -0500

    Complete practice :D

--- Prácticas/2 - Adaline/Adaline.m
+++ Prácticas/2 - Adaline/Adaline.m
@@ -14,6 +14,8 @@ W1 = zeros (1, 9);
 W2 = zeros (1, 9);
 W3 = zeros (1, 9);
 %W = [0.84 0.39 0.78];
+fprintf ('\n\nLos valores iniciales de la matriz de pesos son: ')
+disp (W);
 errores = 0;
 Pesos = figure('Name','Red Adaline - Matriz de Pesos','NumberTitle','off');
 Error = figure('Name','Red Adaline - Señal del error','NumberTitle','off');
@@ -26,6 +28,7 @@ Error_Global = zeros (1, 9);
 for j = 1:it_max
     %k será un 0 cada iteración para convertir a binario
     k = zeros (1, 1);
+    fprintf ('\n\n\nIteración número %d\n', j)
     %Comenzamos a realizar la propagación hacia adelante de todos los datos
     for i = 1:8
         
@@ -42,9 +45,11 @@ for j = 1:it_max
 
         %Realizamos la multiplicacion de las matrices
         a = purelin (W * p);
+        fprintf ('a = %.4f\n', a)
 
         %Obtenemos el error para este dato
         errores = (k - a);
+        fprintf ('error = %.4f\n\n', errores)
 
         %Actualizamos el valor de la matriz de pesos
         W = W + ((2 * alpha * errores) * p');
@@ -78,13 +83,13 @@ for j = 1:it_max
     
     %Si el error global es 0, el aprendizaje obtenido es el ideal
     if Error_It == 0
-        sprintf ('Se cumplió un aprendizaje ideal en la iteración %d', j)
+        fprintf ('Se cumplió un aprendizaje ideal en la iteración %d\n', j)
         break;
     
     %Si el error global es menor al error introducido por el usuario, el 
     %aprendizaje obtenido es 2/3 de exitoso
     elseif Error_It < e_it
-        sprintf ('Se obtuvo un aprendizaje aceptable en la iteración %d', j)
+        fprintf ('Se obtuvo un aprendizaje aceptable en la iteración %d\n', j)
         break;
     end
     
