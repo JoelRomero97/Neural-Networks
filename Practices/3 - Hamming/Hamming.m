b7327b44fff93b4e7bc642ea0c5e0fa6e9ca7373
Joel Romero <joelrg1288@gmail.com>
2017-11-04 11:42:41 -0600

    Se realiza el cálculo de la capa feedforward

--- Practices/3 - Hamming/Hamming.m
+++ Practices/3 - Hamming/Hamming.m
@@ -3,7 +3,7 @@ clc;
 clear;
 
 %Recibimos el nombre del archivo como un String (por eso la 's')
-archivo = input ('Introduce el nombre del archivo: ', 's');
+archivo = input ('Introduce el nombre del archivo que contiene a W: ', 's');
 
 %Abrimos el archivo que contiene la matriz a usar en modo lectura
 archivo_matriz = fopen (archivo, 'r');
@@ -14,10 +14,53 @@ num_col = input ('Ingresa el número de rasgos de cada patron (columas) que tiene
 
 %Creamos una matriz para el bias
 bias = zeros (num_filas,1);
-bias = bias + num_col
+bias = bias + num_col;
 
 while ~feof (archivo_matriz)
-    [aux, cont] = fscanf (archivo_matriz, '%f', [num_col num_filas]);
+    [W, cont] = fscanf (archivo_matriz, '%f', [num_col num_filas]);
+end
+W = W';
+
+clc
+%Recibimos el nombre del archivo como un String (por eso la 's')
+archivo = input ('Introduce el nombre del archivo que contiene el vector de entrada: ', 's');
+
+%Abrimos el archivo que contiene la matriz a usar en modo lectura
+archivo_matriz = fopen (archivo, 'r');
+
+%Guardamos el vector de entrada p en una matriz
+p = fscanf (archivo_matriz, '%f');
+
+%COMIENZA LA CAPA FEEDFORWARD (1 vez)
+a = purelin((W * p) + bias)
+
+
+
+
+
+%ESCRIBIR LA MATRIZ EN UN ARCHIVO DE TEXTO LLAMADO NEW
+
+%Calculamos el tamaño de la matriz W
+[f, c] = size (W);
+nuevo = fopen ('new.txt', 'w');
+
+%Escribimos los datos en un archivo
+for i = 1:f
+    for j = 1:c
+        fprintf (nuevo,'%f\t', W(i, j));
+    end
+      fprintf(nuevo, '\n');
 end
 
-aux = aux'
+
+
+
+
+
+
+%Cerramos los archivos
+fclose (archivo_matriz);
+fclose (nuevo);
+
+%Borramos todas las variables creadas durante la ejecución del programa
+clear;
